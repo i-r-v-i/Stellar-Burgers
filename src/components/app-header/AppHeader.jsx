fefa308e56068d4c170166db4c2ad7ab9d12a8c0
Irina Vinitskaia <kreneva.i@yandex.ru>
2022-12-30 18:10:38 +0300

    курсор в хедере

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -9,27 +9,28 @@ import styles from "./AppHeader.module.css";
 function AppHeader() {
   return (
     <header className={styles.header}>
-      <div className={styles.header__wrapper}>
+      <div className={styles.wrapper}>
         <nav className={`${styles.navList} pb-4 pt-4`}>
-          <div className={`${styles.header__navItem} pb-4 pt-4 pl-5 pr-5`}>
+          <div className={`${styles.navItem} pb-4 pt-4 pl-5 pr-5`}>
             <BurgerIcon type="primary" className="pl-5" />
             <p className="text text_type_main-default pl-2 pr-5">Конструктор</p>
           </div>
           <div
-            className={`${styles.header__navItem} pb-4 pt-4 pl-5 pr-5 text_color_inactive`}
+            className={`${styles.navItem} pb-4 pt-4 pl-5 pr-5 text_color_inactive`}
           >
             <ListIcon type="primary" />
             <p className="text text_type_main-default pl-2">Лента заказов</p>
           </div>
         </nav>
-      
-      <Logo />
-      <div
-        className={`${styles.header__account} pb-4 pt-4 pl-5 pr-5 text_color_inactive`}
-      >
-        <ProfileIcon type="secondary" />
-        <p className="text text_type_main-default pl-2 pr-5">Личный кабинет</p>
-      </div>
+        <Logo />
+        <div
+          className={`${styles.account} pb-4 pt-4 pl-5 pr-5 text_color_inactive`}
+        >
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default pl-2 pr-5">
+            Личный кабинет
+          </p>
+        </div>
       </div>
     </header>
   );
