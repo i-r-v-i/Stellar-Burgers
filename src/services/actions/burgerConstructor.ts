2480390ea5369eed17a9b6d1ea034fd3b2e489f0
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-08-22 15:16:50 +0300

    refactor: add copy branch

--- src/services/actions/burgerConstructor.ts
+++ src/services/actions/burgerConstructor.ts
@@ -3,3 +3,4 @@ export const ADD_BUN: "ADD_BUN" = "ADD_BUN";
 export const DELETE_ITEM: "DELETE_ITEM" = "DELETE_ITEM";
 export const CLEAR_STATE: "CLEAR_STATE" = "CLEAR_STATE";
 export const SORT_ITEM: "SORT_ITEM" = "SORT_ITEM";
+
